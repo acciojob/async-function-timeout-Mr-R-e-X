278c774739241d09721c2868dc2b6bcc0e797adc
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-16 10:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let text = document.querySelector('#text');
+let delay = document.querySelector('#delay');
+let subBtn = document.querySelector('#btn');
+let output = document.querySelector('#output');
+
+function Delay(str, time) {
+	 return new Promise((resolve) => {
+	    setTimeout(() => {
+	      output.innerHTML = str;
+	      resolve();
+	    }, time);
+	  });
+}
+
+async function Display(str, time) {
+	await Delay(str, time)
+}
+
+subBtn.addEventListener("click", (e)=>{
+	e.preventDefault()
+	let str = text.value;
+	let time = parseInt(delay.value, 10) * 1000
+	Display(str, time)
+})
